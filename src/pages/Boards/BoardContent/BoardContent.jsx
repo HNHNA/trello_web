d7ce61193dd4a05db85f605d4eaad4ea5cd32cb8
Hanzdev <huynhngocanhittt@gmail.com>
2024-03-15 17:13:04 +0700

    Drag drop Colomns 1

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,11 +1,51 @@
 import Box from '@mui/material/Box'
 import ListColumn from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import { DndContext } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
+
 
 function BoardContent({ board } ) {
-  const orderedColumns = mapOrder(board?.columns, board.columnOrderIds, '_id')
+  //Sắp xếp Columns và kéo thả dnd-kit
+  const [orderedColumns, setOerderedColumns] = useState([])
+
+  useEffect(() => {
+    //const orderedColumns = mapOrder(board?.columns, board.columnOrderIds, '_id')
+    setOerderedColumns(mapOrder(board?.columns, board.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    console.log('handleDragEnd:', event)
+    const { active, over } = event
+
+    // Kiểm tra nếu khồn tồn tại over (kéo linh tinh ra ngoài thì return luôn tránh lỗi)
+    if (!over) return
+
+    // Nếu vị trí sau khi kéo thả khác với vị trí ban đầu
+    if (active.id !== over.id) {
+      // Lấy vị trí cũ (từ thằng active)
+      const oldIndex = orderedColumns.findIndex ( c => c._id === active.id)
+      // Lấy vị trí mới (từ thằng over)
+      const newIndex = orderedColumns.findIndex ( c => c._id === over.id)
+
+      //Dùng arayMove của dnd-kit để sắp xếp lại Column ban đầu
+      // Code arayMove: https://github.com/clauderic/dnd-kit/blob/master/packages/sortable/src/utilities/arrayMove.ts
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      //2 console.log này để sau dùng để xử lý gọi API
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      //console.log('dndOrderedColumns:', dndOrderedColumns)
+      // console.log('dndOrderedColumnsIds:', dndOrderedColumnsIds)
+
+      //Cập nhật lại state column ban đầu sau khi đã kéo thả
+      setOerderedColumns(dndOrderedColumns)
+    }
+
+  }
+
+
   return (
-    <div>
+    <DndContext onDragEnd={handleDragEnd}>
       {/* Box Column */}
       <Box sx={{
         bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
@@ -15,7 +55,7 @@ function BoardContent({ board } ) {
       }}>
         <ListColumn columns={orderedColumns} />
       </Box>
-    </div>
+    </DndContext>
   )
 }
 
